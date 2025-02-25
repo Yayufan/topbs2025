a379a2eae2a65060637f26da01bba170ab748377
Yayufan <kamikazey2200@gmail.com>
2025-02-25 16:21:40 +0800

    update Article and ArticleAttachment function

---
+++ topbs2025-admin/src/main/java/tw/com/topbs/convert/ArticleAttachmentConvert.java
@@ -0,0 +1,11 @@
+package tw.com.topbs.convert;
+
+import org.mapstruct.Mapper;
+
+import tw.com.topbs.pojo.DTO.addEntityDTO.AddArticleAttachmentDTO;
+import tw.com.topbs.pojo.entity.ArticleAttachment;
+
+@Mapper(componentModel = "spring")
+public interface ArticleAttachmentConvert {
+	ArticleAttachment insertDTOToEntity(AddArticleAttachmentDTO insertArticleAttachmentDTO);
+}
--- topbs2025-admin/src/main/java/tw/com/topbs/service/ArticleAttachmentService.java
+++ topbs2025-admin/src/main/java/tw/com/topbs/service/ArticleAttachmentService.java
@@ -1,16 +1,56 @@
 package tw.com.topbs.service;
 
-import tw.com.topbs.pojo.entity.ArticleAttachment;
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import tw.com.topbs.pojo.DTO.addEntityDTO.AddArticleAttachmentDTO;
+import tw.com.topbs.pojo.entity.ArticleAttachment;
 
 /**
  * <p>
- * 文章附件表 服务类
+ * 文章的附件 服务类
  * </p>
  *
  * @author Joey
- * @since 2025-02-05
+ * @since 2024-12-27
  */
 public interface ArticleAttachmentService extends IService<ArticleAttachment> {
+
+	/**
+	 * 根據文章ID,獲取該文章的所有附件
+	 * 
+	 * @param articleId
+	 * @return
+	 */
+	List<ArticleAttachment> getAllArticleAttachmentByArticleId(Long articleId);
+
+	/**
+	 * 根據文章ID,獲取該文章的所有附件(分頁)
+	 * 
+	 * @param articleId
+	 * @param page
+	 * @return
+	 */
+	IPage<ArticleAttachment> getAllArticleAttachmentByArticleId(Long articleId, Page<ArticleAttachment> page);
+
+	/**
+	 * 新增文章附件
+	 * 
+	 * @param insertArticleDTO
+	 */
+	void insertArticleAttachment(AddArticleAttachmentDTO insertArticleAttachmentDTO, MultipartFile[] files);
+
+	
+	/**
+	 * 根據articleAttachmentId刪除文章附件
+	 * 
+	 * @param articleAttachmentId
+	 */
+	void deleteArticleAttachment(Long articleAttachmentId);
 
 }
